538f6cddafc679e0d14485c0a095fc866fc7e22d
Peepsdub <peepsdub@gmail.com>
2024-03-27 14:56:49 -0600

    done server.py

--- backend/server.py
+++ backend/server.py
@@ -20,4 +20,23 @@ def get_products(product_id=None):
         product = next((p for p in products if p['id'] == product_id), None)
         #If a specific product is requested,
         #wrap it in an object with a 'products' key
-        #
+        #Note: you might want to change this
+        #if you want to return a single product not wrapped in a list
+        return jsonify(product) if product else ('', 404)
+    
+@app.route('/products/add', methods=['POST'])
+def add_product():
+    new_product = request.json
+    products = load_products()
+    new_product['id'] = len(products) + 1
+    products.append(new_product)
+    with open('products.json', 'w') as f:
+        json.dump({"products": products}, f)
+    return jsonify(new_product), 201
+
+@app.route('/product-images/<path:filename>')
+def get_image(filename):
+    return send_from_directory('product-images', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
